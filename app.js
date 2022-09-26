c4d55ad5dfc9cae943df0790e6abd9a980f19441
thunder@1234x <rakeshbasak53@gmail.com>
2022-09-26 21:32:19 +0530

    Starting Express
    Feeling Interesting

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const customEvent = require('./Event');
+// const customEvent = require('./Event');
 
-customEvent.emit('data');
-customEvent.emit('info','Rakesh',23);
+// customEvent.emit('data');
+// customEvent.emit('info','Rakesh',23);
+
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.status(200).json({status:'success',code:200});
+})
+
+app.listen(5000,()=>{
+    console.log("Server is listening on port 5000 ..........");
+})
